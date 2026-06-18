41c14ed9d29e8fc75815d4f8d28281842abbf760
bassant-mahmoud-sallam <bassantsallam13@gmail.com>
2026-06-19 01:00:35 +0300

    favourites_page

--- WebMart_Project/WebMart/src/app/layouts/admin-layout/admin-layout.ts
+++ WebMart_Project/WebMart/src/app/layouts/admin-layout/admin-layout.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
+import { AdminSidebar } from "../../shared/components/admin-sidebar/admin-sidebar";
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
+  imports: [RouterOutlet, AdminSidebar],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.scss',
 })
--- WebMart_Project/WebMart/src/app/layouts/user-layout/user-layout.ts
+++ WebMart_Project/WebMart/src/app/layouts/user-layout/user-layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../../shared/components/navbar/navbar";
+import { RouterOutlet } from "@angular/router";
+import { Footer } from "../../shared/components/footer/footer";
 
 @Component({
   selector: 'app-user-layout',
-  imports: [],
+  imports: [Navbar, RouterOutlet, Footer],
   templateUrl: './user-layout.html',
   styleUrl: './user-layout.scss',
 })
