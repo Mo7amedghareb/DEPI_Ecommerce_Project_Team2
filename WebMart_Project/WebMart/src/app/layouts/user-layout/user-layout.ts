ed0698776b5ac83335841b193116ee46ee3e1409
Mo7amedghareb <modyghareb2017@gmail.com>
2026-06-23 20:11:00 +0300

    add import on user layout

--- WebMart_Project/WebMart/src/app/layouts/user-layout/user-layout.ts
+++ WebMart_Project/WebMart/src/app/layouts/user-layout/user-layout.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Navbar } from '../../shared/components/navbar/navbar';
 import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-user-layout',
-  imports: [RouterOutlet, RouterLink, Navbar, Footer],
+  imports: [RouterOutlet,RouterLink , Navbar, Footer],
   templateUrl: './user-layout.html',
   styleUrl: './user-layout.scss',
 })
