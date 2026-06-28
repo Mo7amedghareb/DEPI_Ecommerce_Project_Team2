0437f35333e9498ea72df00a5e868f6d4f3180c3
Asmaa Hassan <ah687320@gmail.com>
2026-06-29 02:41:15 +0300

    Save layout and order details changes

--- WebMart_Project/WebMart/src/app/layouts/admin-layout/admin-layout.ts
+++ WebMart_Project/WebMart/src/app/layouts/admin-layout/admin-layout.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AdminSidebar } from "../../shared/components/admin-sidebar/admin-sidebar";
+import { AdminSidebar } from '../../shared/components/admin-sidebar/admin-sidebar';
+
+import { AddEditProduct } from '../../features/admin/pages/add-edit-product/add-edit-product'; 
+
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, AdminSidebar],
+ 
+  imports: [RouterOutlet, AdminSidebar, AddEditProduct], 
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.scss',
 })
